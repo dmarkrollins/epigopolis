71931b31594278a396106cfc4dc2bf7b17daffb0
ddross <DDROSS@travelers.com>
2017-06-02 14:24:19 -0400

    wip

--- MongoLink/index.js
+++ MongoLink/index.js
@@ -1,32 +1,27 @@
 'use strict';
-
 const MongoClient = require('mongodb').MongoClient;
-let cachedDb = null;
+var db = require("./src/dbInteractions.js");
 
-exports.handler = function(event, context, callback){
+exports.handler = function (event, context, callback) {
     processEvent(event, context, callback);
 }
 
 function processEvent(event, context, callback) {
     var uri = process.env['MONGODB_ATLAS_CLUSTER_URI'];
     context.callbackWaitsForEmptyEventLoop = false;
-    
+
     try {
-        //testing if the database connection exists and is connected to Atlas so we can try to re-use it
-        if (cachedDb && cachedDb.serverConfig.isConnected()) {
-            createDoc(cachedDb, event, callback);
-        }
-        else {
-            //some performance penalty might be incurred when running that database connection initialization code
-            console.log(`=> connecting to database ${uri}`);
-            MongoClient.connect(uri, function (err, db) {
-                if (err) {
-                    console.log("the error is ${err}.", err)
-                    callback("Unable to connect to database");
-                }
-                cachedDb = db;
-                return createDoc(db, event, callback);
-            });            
+        switch (event.transaction) {
+            case "add":
+                db.addDocument(event.data, function (err, result) {
+                    if (err) {
+                        console.log(err);
+                        callback();
+                    }
+                    console.log(result);
+                    callback();
+                });
+                break;
         }
     }
     catch (err) {
@@ -35,20 +30,35 @@ function processEvent(event, context, callback) {
     }
 }
 
-function createDoc(db, json, callback) {
-    console.log('creating doc');
-    try {
-        cachedDb.collection('Sessions').insertOne(json, function (err, result) {
-            if (err !== null) {
-                console.error("an error occurred in createDoc", err);
-                callback(err);
-            }
-            else {
-                console.log("Document created with id: " + result.insertedId);
-                callback(null, "Document created with id: " + result.insertedId);
-            }
-        });
-    } catch (ex) {
-        console.log(ex);
-    }
+function queryDocument(callback) {
+    db.queryDocument({ "contextId": "qwerty123" }, function (err, document) {
+        if (err) {
+            console.log(err);
+            callback();
+        }
+        console.log(document);
+        callback();
+    });
+}
+
+function updateDocument(callback) {
+    db.updateDocument("592d448ca593110879999de5", { "with": "update from node js 3" }, function (err, document) {
+        if (err) {
+            console.log(err);
+            callback();
+        }
+        console.log(document);
+        callback();
+    });
+}
+
+function getDocumentById(callback) {
+    db.getDocumentById("592d448ca593110879999de5", function (err, document) {
+        if (err) {
+            console.log(err);
+            callback();
+        }
+        console.log(document);
+        callback();
+    });
 }
