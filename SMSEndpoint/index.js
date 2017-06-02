406d794af637c80dd986aee7814289f6904c9cb2
Dave Rollins <drollin2@travelers.com>
2017-06-02 14:22:05 -0400

    WIP

--- SMSEndpoint/index.js
+++ SMSEndpoint/index.js
@@ -1,9 +1,6 @@
 const botBuilder = require('claudia-bot-builder');
 
-const bot = botBuilder(function(message) {
-    const response = `Thanks for sending '${message.text}'... your message is very important to us but something happened ...`;
-    console.log(response);
-    return response;
-});
-
-module.exports = bot;
+module.exports = botBuilder(function(message) {
+    console.log(JSON.stringify(message));
+    return `${message.text} to you too`;
+}, { platforms: ['twilio'] });
